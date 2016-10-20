f16c4d148c6000be1e5a55863d2abc8ecce991dd
Adam Cooper <adamcooperrc@gmail.com>
2016-10-20 07:33:59 -0400

    Hid secret key from settings

--- src/Django/vipebot5000/settings.py
+++ src/Django/vipebot5000/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from . import django_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vc%l-b$(r#1@(tp8=r*u1@i3tr^uj6_f87p_jw3c5gfs_d9v47'
+SECRET_KEY = django_secret_key.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
